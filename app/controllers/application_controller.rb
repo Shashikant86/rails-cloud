27bc1eb66f5e3354a19fedc0913dd872fb08543f
Shashikant86 <shashikant.jagtap@aol.co.uk>
2015-12-14 20:29:08 +0000

    Updated rendering html

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,6 +3,6 @@ class ApplicationController < ActionController::Base
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
   def hello
-    render text: "Hello World"
+    render html: "<strong>Rails App Developed by Shashikant</strong>".html_safe
   end   
 end
